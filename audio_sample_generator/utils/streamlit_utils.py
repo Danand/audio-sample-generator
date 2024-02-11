c0e2127f5eb7126ded3e57dc35872175d4fecd74
Andrey Danilov <danand@inbox.ru>
2024-02-12 00:50:49 +0300

    Introduce `get_spectrogram_to_audio_settings()`

--- audio_sample_generator/utils/streamlit_utils.py
+++ audio_sample_generator/utils/streamlit_utils.py
@@ -1,8 +1,9 @@
 from audio_sample_generator.data.sample_data import SampleData
+from audio_sample_generator.utils.torch_utils import get_window_fn_dict
 
 import streamlit as st
 
-from typing import List
+from typing import Callable, List, cast
 
 SESSION_STATE_KEY_SAMPLE_DATA_LIST = "sample_data_list"
 
@@ -10,3 +11,216 @@ if st.session_state.get(SESSION_STATE_KEY_SAMPLE_DATA_LIST) is None:
     st.session_state[SESSION_STATE_KEY_SAMPLE_DATA_LIST] = []
 
 sample_data_list: List[SampleData] = st.session_state[SESSION_STATE_KEY_SAMPLE_DATA_LIST]
+
+class SpectrogramToAudioSettings:
+    sample_rate: int
+    f_min: float
+    f_max: float | None
+    norm: str | None
+    n_fft: int
+    n_iter: int
+    win_length: int | None
+    hop_length: int
+    n_mels: int
+    window_fn: Callable
+    mel_scale: str
+    driver: str
+    power: float
+    momentum: float
+    length: int | None
+    rand_init: bool
+
+def get_spectrogram_to_audio_settings() -> SpectrogramToAudioSettings:
+    st.subheader("Mel Spectrogram Settings")
+
+    settings = SpectrogramToAudioSettings()
+
+    settings.sample_rate = cast(
+        int,
+        st.selectbox(
+            label="Sample rate",
+            options=[
+                8000,
+                11025,
+                16000,
+                22050,
+                44100,
+                48000,
+            ],
+            index=3,
+        )
+    )
+
+    settings.f_min = cast(
+        float,
+        st.number_input(
+            label="Minimum Frequency",
+            min_value=0.0,
+            value=0.0,
+        )
+    )
+
+    custom_value_enabled_f_max = st.checkbox(
+        label="Custom Maximum Frequency",
+        value=False,
+    )
+
+    settings.f_max = cast(
+        float,
+        st.number_input(
+            label="Maximum Frequency",
+            min_value=0.0,
+            value=float(settings.sample_rate // 2),
+            step=1.0,
+            disabled=not custom_value_enabled_f_max,
+        )
+    )
+
+    if not custom_value_enabled_f_max:
+        settings.f_max = None
+
+    settings.norm = st.selectbox(
+        label="Area Normalization",
+        options=[
+            None,
+            "slaney",
+        ],
+        index=0,
+    )
+
+    settings.n_fft = cast(
+        int,
+        st.number_input(
+            label="FFT Length",
+            min_value=0,
+            value=400,
+        )
+    )
+
+    settings.n_iter = cast(
+        int,
+        st.number_input(
+            label="Phase Recovery Iterations",
+            min_value=1,
+            value=32,
+        )
+    )
+
+    custom_value_enabled_win_length = st.checkbox(
+        label="Custom Window Length",
+        value=False,
+    )
+
+    settings.win_length = cast(
+        int,
+        st.number_input(
+            label="Window Length",
+            min_value=0,
+            value=400,
+            disabled=not custom_value_enabled_win_length,
+        )
+    )
+
+    if not custom_value_enabled_win_length:
+        settings.win_length = None
+
+    settings.hop_length = cast(
+        int,
+        st.number_input(
+            label="Hop Length",
+            min_value=0,
+            value=400,
+        )
+    )
+
+    settings.n_mels = cast(
+        int,
+        st.number_input(
+            label="Mel filter banks number",
+            min_value=0,
+            value=128,
+        )
+    )
+
+    window_fn_dict = get_window_fn_dict()
+
+    window_fn_key = cast(
+        str,
+        st.selectbox(
+            label="Window Function",
+            options=window_fn_dict.keys(),
+            index=0,
+        )
+    )
+
+    settings.window_fn = window_fn_dict[window_fn_key]
+
+    settings.mel_scale = cast(
+        str,
+        st.selectbox(
+            label="Mel Scale",
+            options=[
+                "htk",
+                "slaney",
+            ],
+            index=0,
+        )
+    )
+
+    settings.driver = cast(
+        str,
+        st.selectbox(
+            label="Driver",
+            options=[
+                "gels",
+                "gelsy",
+                "gelsd",
+                "gelss",
+            ],
+            index=1,
+        )
+    )
+
+    settings.power = cast(
+        float,
+        st.number_input(
+            label="Power",
+            min_value=0.0,
+            value=1.0,
+        )
+    )
+
+    settings.momentum = cast(
+        float,
+        st.number_input(
+            label="Momentum",
+            min_value=0.0,
+            value=0.99,
+            help="The momentum parameter for fast Griffin-Lim. Setting this to 0 recovers the original Griffin-Lim method. Values near 1 can lead to faster convergence, but above 1 may not converge.",
+        )
+    )
+
+    custom_value_enabled_length = st.checkbox(
+        label="Custom Length",
+        value=False,
+    )
+
+    settings.length = cast(
+        int,
+        st.number_input(
+            label="Length",
+            min_value=0,
+            value=int(settings.sample_rate / settings.hop_length),
+            disabled=not custom_value_enabled_length,
+        )
+    )
+
+    if not custom_value_enabled_length:
+        settings.length = None
+
+    settings.rand_init = st.checkbox(
+        label="Random Phase",
+        value=True,
+    )
+
+    return settings
--- pages/5_Batch_Convert_to_Audio.py
+++ pages/5_Batch_Convert_to_Audio.py
@@ -1,4 +1,4 @@
-from audio_sample_generator.utils.torch_utils import get_window_fn_dict
+from audio_sample_generator.utils.streamlit_utils import get_spectrogram_to_audio_settings
 
 import torchvision
 import torchaudio
@@ -6,8 +6,6 @@ import torchaudio
 from PIL import Image
 
 import streamlit as st
-
-from typing import cast
 
 title = "Batch Convert Spectrograms To Audio"
 
@@ -28,195 +26,7 @@ input_image_files = st.file_uploader(
 if input_image_files is not None \
     and len(input_image_files) > 0:
     with st.container(border=True):
-        st.subheader("Mel Spectrogram Settings")
-
-        sample_rate = cast(
-            int,
-            st.selectbox(
-                label="Sample rate",
-                options=[
-                    8000,
-                    11025,
-                    16000,
-                    22050,
-                    44100,
-                    48000,
-                ],
-                index=3,
-            )
-        )
-
-        f_min = cast(
-            float,
-            st.number_input(
-                label="Minimum Frequency",
-                min_value=0.0,
-                value=0.0,
-            )
-        )
-
-        custom_value_enabled_f_max = st.checkbox(
-            label="Custom Maximum Frequency",
-            value=False,
-        )
-
-        f_max = cast(
-            float,
-            st.number_input(
-                label="Maximum Frequency",
-                min_value=0.0,
-                value=float(sample_rate // 2),
-                step=1.0,
-                disabled=not custom_value_enabled_f_max,
-            )
-        )
-
-        if not custom_value_enabled_f_max:
-            f_max = None
-
-        norm = st.selectbox(
-            label="Area Normalization",
-            options=[
-                None,
-                "slaney",
-            ],
-            index=0,
-        )
-
-        n_fft = cast(
-            int,
-            st.number_input(
-                label="FFT Length",
-                min_value=0,
-                value=400,
-            )
-        )
-
-        n_iter = cast(
-            int,
-            st.number_input(
-                label="Phase Recovery Iterations",
-                min_value=1,
-                value=32,
-            )
-        )
-
-        custom_value_enabled_win_length = st.checkbox(
-            label="Custom Window Length",
-            value=False,
-        )
-
-        win_length = cast(
-            int,
-            st.number_input(
-                label="Window Length",
-                min_value=0,
-                value=400,
-                disabled=not custom_value_enabled_win_length,
-            )
-        )
-
-        if not custom_value_enabled_win_length:
-            win_length = None
-
-        hop_length = cast(
-            int,
-            st.number_input(
-                label="Hop Length",
-                min_value=0,
-                value=400,
-            )
-        )
-
-        n_mels = cast(
-            int,
-            st.number_input(
-                label="Mel filter banks number",
-                min_value=0,
-                value=128,
-            )
-        )
-
-        window_fn_dict = get_window_fn_dict()
-
-        window_fn_key = cast(
-            str,
-            st.selectbox(
-                label="Window Function",
-                options=window_fn_dict.keys(),
-                index=0,
-            )
-        )
-
-        mel_scale = cast(
-            str,
-            st.selectbox(
-                label="Mel Scale",
-                options=[
-                    "htk",
-                    "slaney",
-                ],
-                index=0,
-            )
-        )
-
-        driver = cast(
-            str,
-            st.selectbox(
-                label="Driver",
-                options=[
-                    "gels",
-                    "gelsy",
-                    "gelsd",
-                    "gelss",
-                ],
-                index=1,
-            )
-        )
-
-        window_fn = window_fn_dict[window_fn_key]
-
-        power = cast(
-            float,
-            st.number_input(
-                label="Power",
-                min_value=0.0,
-                value=1.0,
-            )
-        )
-
-        momentum = cast(
-            float,
-            st.number_input(
-                label="Momentum",
-                min_value=0.0,
-                value=0.99,
-                help="The momentum parameter for fast Griffin-Lim. Setting this to 0 recovers the original Griffin-Lim method. Values near 1 can lead to faster convergence, but above 1 may not converge.",
-            )
-        )
-
-        custom_value_enabled_length = st.checkbox(
-            label="Custom Length",
-            value=False,
-        )
-
-        length = cast(
-            int,
-            st.number_input(
-                label="Length",
-                min_value=0,
-                value=int(sample_rate / hop_length),
-                disabled=not custom_value_enabled_length,
-            )
-        )
-
-        rand_init = st.checkbox(
-            label="Random Phase",
-            value=True,
-        )
-
-        if not custom_value_enabled_length:
-            length = None
+        settings = get_spectrogram_to_audio_settings()
 
         if st.button(
             label="Convert",
@@ -227,14 +37,14 @@ if input_image_files is not None \
                 st.subheader("Converted Audio")
 
                 transform_inverse_mel_scale = torchaudio.transforms.InverseMelScale(
-                    n_stft=n_fft // 2 + 1,
-                    n_mels=n_mels,
-                    sample_rate=sample_rate,
-                    f_min=f_min,
-                    f_max=f_max,
-                    norm=norm,
-                    mel_scale=mel_scale,
-                    driver=driver,
+                    n_stft=settings.n_fft // 2 + 1,
+                    n_mels=settings.n_mels,
+                    sample_rate=settings.sample_rate,
+                    f_min=settings.f_min,
+                    f_max=settings.f_max,
+                    norm=settings.norm,
+                    mel_scale=settings.mel_scale,
+                    driver=settings.driver,
                 )
 
                 transform_image_tensor = torchvision.transforms.Compose([
@@ -243,15 +53,15 @@ if input_image_files is not None \
                 ])
 
                 transform_griffin_lim = torchaudio.transforms.GriffinLim(
-                    n_fft=n_fft,
-                    n_iter=n_iter,
-                    win_length=win_length,
-                    hop_length=hop_length,
-                    window_fn=window_fn,
-                    power=power,
-                    momentum=momentum,
-                    length=length,
-                    rand_init=rand_init,
+                    n_fft=settings.n_fft,
+                    n_iter=settings.n_iter,
+                    win_length=settings.win_length,
+                    hop_length=settings.hop_length,
+                    window_fn=settings.window_fn,
+                    power=settings.power,
+                    momentum=settings.momentum,
+                    length=settings.length,
+                    rand_init=settings.rand_init,
                 )
 
                 for input_image_file in input_image_files:
@@ -266,7 +76,7 @@ if input_image_files is not None \
 
                         st.audio(
                             data=waveform.numpy(),
-                            sample_rate=sample_rate,
+                            sample_rate=settings.sample_rate,
                         )
 
                         st.image(
